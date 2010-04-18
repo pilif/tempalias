1144de6b4c01e99fcd78d8c91c81d6be6a6635e5
Philip Hofstetter <phofstetter@sensational.ch>
2010-04-19 00:03:41 +0200

    drop express for paperboy
    see blog post for reasoning, but here's the executive summary:
    
    - express did too much (too much abstraction, too many dependencies, too
      many features. none needed) and too little (little help for webservice
      like requests)
    - express had a strange issue that forced node not to cleanly close a
      connection if the request body was null (for 304: not modified)
    
    The nice thing about paperboy is that it doesnt try to be rails, but
    it's just a plug-in solution to plug static file serving as a feature of
    the webserver you are creating.
    
    This is SO MUCH nicer.
    
    And the rewrite isn't that big either

--- tempalias_http.js
+++ tempalias_http.js
@@ -1,55 +1,96 @@
 require.paths.unshift('lib');
-require.paths.unshift('deps/express/lib');
+require.paths.unshift('deps/node-paperboy/lib');
 require.paths.unshift('deps/redis-node-client/lib');
-require('express');
 
 var sys = require('sys');
+var http = require('http');
+
 var tempalias = require('tempalias');
 var config = require('config');
+var paperboy = require('paperboy');
+
 
 var p = tempalias.AliasProvider;
 
-configure('development', function(){
-    enable('show exceptions');
-    enable('throw exceptions');
-});
-
-
-post('/aliases', function(){
-    var self = this;
-
-    this.contentType('application/json');
-    if (this.headers['content-type'] != 'application/json'){
-        this.halt(400, JSON.stringify({
-            error: "invalid-request-type",
-            description: "invalid request type. Need application/json"
-        }));
-    }
-    var len = this.headers['content-length'] || 1024;
-    if (len >= 1024){
-        this.halt(400, JSON.stringify({
-            error: "invalid-content-length",
-            description: "no or too big content length provided"
-
-        }));
-    }
-    var alias = p.getNew();
-    try{
-        alias.setInfo(JSON.parse(this.body));
-    }catch(e){
-        this.halt(400, JSON.stringify({
-            error: "invalid-data",
-            description: e.toString()
-        }));
-    }
-    p.save(alias, function(id){
-        self.halt(200, JSON.stringify(alias));
-    })
-});
 
 require('redis').client.stream.addListener("connect", function (){
-  run(
-    config.http.port || 8080,
-    config.http.listen || undefined
-  );
+
+  http.createServer(function(req, res) {
+    if (req.url == '/aliases'){
+      var headers = {"Content-Type": "application/json"};
+      var err = function(code, resp){
+        var body = JSON.stringify(resp);
+        headers['Content-Length'] = body.length;
+        res.writeHead(code, headers);
+        res.end(body);
+      }
+
+      if (req.method != 'POST'){
+        err(405, {
+          error: "method-not-allowed",
+          description: '/aliases only takes POST'
+        });
+        return;
+      }
+
+      if (req.headers['content-type'] != 'application/json'){
+        err(400, {
+          error: "invalid-request-type",
+          description: "invalid request type. Need application/json"
+        });
+        return;
+      }
+      var len = req.headers['content-length'] || 1024;
+      if (len >= 1024){
+        err(400 , {
+          error: "invalid-content-length",
+          description: "no or too big content length provided"
+        });
+      }
+      var reqbody = '';
+      var err = false;
+      req.addListener('data', function(chunk){
+        reqbody += chunk;
+        if (reqbody.length >= 1024){
+          err = true;
+          err(400, { error: "too-much-data", description: "request too big"});
+        }
+      });
+      req.addListener('end', function(){
+        if (err) return;
+        var alias = p.getNew();
+        try{
+            alias.setInfo(JSON.parse(reqbody));
+        }catch(e){
+            err(400, {
+                error: "invalid-data",
+                description: e.toString()
+            });
+        }
+        p.save(alias, function(id){
+          var body = JSON.stringify(alias);
+          headers['Content-Length'] = body.length;
+          res.writeHead(200, headers);
+          res.end(body);
+        });
+      });
+      return;
+    }
+
+    // other cases: Divert to paperboy
+    paperboy
+        .deliver(__dirname + '/public', req, res)
+        .error(function(statCode,msg) {
+          res.writeHead(statCode, {'Content-Type': 'text/plain'});
+          res.write("Error: " + statCode);
+          res.close();
+          sys.debug("error");
+        })
+        .otherwise(function(err) {
+          var statCode = 404;
+          res.writeHead(statCode, {'Content-Type': 'text/plain'});
+          res.end('not found');
+        });
+  }).listen(config.http.port, config.http.listen);
+  sys.puts('tempalias HTTP daemon running on port '+config.http.port);
 });
