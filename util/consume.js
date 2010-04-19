ab71b9be97900d223bb7a36fcc6a3b9e95748e7f
Philip Hofstetter <phofstetter@sensational.ch>
2010-04-20 01:01:25 +0200

    fix testscript for new redis method

--- util/consume.js
+++ util/consume.js
@@ -5,7 +5,7 @@ var sys = require('sys');
 var aliasProvider = require('tempalias').AliasProvider;
 var client = require('redis').client;
 
-require('redis').client.stream.addListener("connect", function (){
+require('redis').client(function(client){
   aliasProvider.findById(process.argv[2], true, function(alias){
     sys.p(alias);
     client.close();
