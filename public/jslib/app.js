be02a20e4d9e026f3f5191db3f2619f85bbe8ec9
Philip Hofstetter <phofstetter@sensational.ch>
2010-04-21 22:32:09 +0200

    sammyfying
    - rendering the form
    - rendering the about page
    - generic page rendering framework

---
+++ public/jslib/app.js
@@ -0,0 +1,42 @@
+Tempalias = {};
+
+(function($) {
+  Tempalias.app = $.sammy((function(){
+    return function(){
+      this.helpers({
+        e: function(t){
+          t = t || '';
+          return t.replace(/&/g, "&amp;")
+              .replace(/</g, "&lt;")
+              .replace(/>/g, "&gt;")
+              .replace(/"/g, "&quot");
+        }
+      });
+      this.element_selector = '#playground';
+      this.use(Sammy.Template);
+      this.template_engine = 'Sammy.Template';
+
+      this.get('#/', function(context){
+        this.partial('templates/form.template', {target: null, days: null, maxUsage: null});
+      });
+
+      this.get('#/:page', function(context){
+        $.ajax({
+          url: 'templates/'+this.params['page']+'.html',
+          dataType: 'html',
+          success: function(data) {
+            context.$element().html(data);
+          },
+          error: function(){
+            console.log(arguments);
+            context.partial('templates/notfound.html');
+          }
+        });
+      });
+
+    };
+  })());
+  $(function() {
+    Tempalias.app.run('#/');
+  });
+})(jQuery);
