fec13e8dbcea4a04201076344b690d1f66d6809a
Philip Hofstetter <phofstetter@sensational.ch>
2010-04-22 01:20:37 +0200

    implemented the alias generation form!
    now we just need to fix the positive result display and we're done!

--- public/jslib/app.js
+++ public/jslib/app.js
@@ -27,6 +27,18 @@ Tempalias = {};
 
       this.get('#/', function(context){
         this.partial('templates/form.template', {target: null, days: null, maxUsage: null});
+        this.app.last_location = null;
+      });
+
+      this.get('#/form', function(context){
+        this.app.last_location = null;
+        this.app.setLocation('#/');
+        this.app.trigger('location-changed');
+      });
+
+      this.get('#/buildalias', function(context){
+        context.$element().html('gnegg');
+
       });
 
       this.get('#/:page', function(context){
@@ -41,6 +53,46 @@ Tempalias = {};
             context.partial('templates/notfound.html');
           }
         });
+      });
+      this.post('#/buildalias', function(context) {
+        this.app.last_location = null;
+        
+        var valn = function(i){
+          var t = parseInt(i, 10);
+          return isNaN(t) ? 0 : t;
+        };
+        var vale = function(e){
+          return e.match(/^[^@]+@[^.@]+\.[^@.][^@]+$/) ? e : '';
+        };
+        var alias = {
+          target: vale($('#target').val()),
+          days:  valn($('#days').val()),
+          "max-usage": valn($('#max-usage').val())
+        };
+        if (!alias.target || (!alias.days && !alias['max-usage'])){
+          console.log(alias);
+          var e = $('#error');
+          e.show();
+          setTimeout(function(){e.fadeOut('slow');}, 1000);
+          return;
+        }
+
+        $.ajax({
+          url: '/aliases',
+          type: 'POST',
+          dataType: 'json',
+          processData: false,
+          contentType: 'application/json',
+          data: JSON.stringify(alias),
+          success: function(data) {
+             context.partial('templates/result.template', data);
+          },
+          error: function(req){
+            var err = JSON.parse(req.responseText);
+            err.code = req.status;
+            context.partial('templates/error.template', err);
+           }
+         });
       });
 
     };
